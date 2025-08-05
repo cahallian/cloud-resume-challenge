1a30ed97008899fb4a364fa5f5361372e87a1286
Ian <53545215+cahallian@users.noreply.github.com>
2025-08-05 12:51:13 -0400

    CORS pt 2

--- guestbook/app.py
+++ guestbook/app.py
@@ -12,9 +12,9 @@ def lambda_handler(event, context):
         return {
             'statusCode': 200,
             'headers': {
-                'Access-Control-Allow-Origin': 'https://ian-cahall-resume.com',
-                'Access-Control-Allow-Methods': 'POST,OPTIONS',
-                'Access-Control-Allow-Headers': 'Content-Type'
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': '*',
+                'Access-Control-Allow-Headers': '*'
             },
             'body': json.dumps({'message': 'CORS preflight'})
         }
